1bf04c99abe230b1e2e1f85b862290b68021da31
Ali Hamza Sultan <122203689+alihamzasultan@users.noreply.github.com>
2025-02-08 13:21:53 +0500

    Add files via upload

---
+++ app/(auth)/sign-in/signin/page.tsx
@@ -0,0 +1,19 @@
+// import { SignIn } from '@clerk/nextjs'
+
+// export default function Page() {
+//   return (
+//    <div className="flex justify-center items-center h-screen">
+//         <div
+//                      style={{
+//                        display: "flex",
+//                        justifyContent: "center",
+//                        alignItems: "center",
+//                        height: "100vh",
+//                        backgroundColor: "#f9f9f9",
+//                      }}
+//                    >
+//                      <SignIn routing="hash" />
+//                    </div>
+//       </div>
+//   )
+// }
---
+++ app/(auth)/sign-up/signup/page.tsx
@@ -0,0 +1,22 @@
+// import { SignUp } from '@clerk/nextjs'
+
+// export default function Page() {
+    
+//   return (
+//    <div
+//              style={{
+//                display: "flex",
+//                justifyContent: "center",
+//                alignItems: "center",
+//                height: "100vh",
+//                backgroundColor: "#f9f9f9",
+//              }}
+//            >
+//              <SignUp routing="hash" />
+//            </div>
+    
+  
+  
+
+//   );
+// }
